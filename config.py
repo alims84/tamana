cca115c16337c77f37968496f41b5ed4cb2ad92f
alims84 <aryatrading.biz@gmail.com>
2025-12-07 11:16:53 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+# ============================
+#          CONFIG.PY
+# ============================
+
+BOT_TOKEN = "8437924316:AAFysR4_YGYr2HxhxLHWUVAJJdNHSXxNXns"
+
+# مدیر اصلی
+ADMINS = {5132329814}   # ID خودت
+
+# واتساپ و اینستاگرام
+WHATSAPP_NUMBER = "989190432181"
+INSTAGRAM_URL = "https://instagram.com/tamara.clinic"   # تستی
+
+# نام کلینیک
+CLINIC_NAME = "کلینیک زیبایی تمارا"
+CLINIC_ADDRESS = "دماوند - خیابان بهشتی"
+
+# شماره کارت پرداخت آفلاین
+CARD_NUMBER = "6037991234567890"
+
+# Webhook URL — مخصوص Render
+WEBHOOK_URL = "https://tamara.onrender.com/webhook"
