516ad48a75e369af4b6bad2b4321c6487fbf773a
alims84 <aryatrading.biz@gmail.com>
2025-12-07 13:41:09 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,11 @@
-# ============================
-#         CONFIG
-# ============================
 
 BOT_TOKEN = "8437924316:AAFysR4_YGYr2HxhxLHWUVAJJdNHSXxNXns"
 
-ADMINS = {5132329814}   # ادمین‌ها
-SECRETARIES = set()     # منشی‌ها (قابل اضافه کردن بعداً)
+ADMINS = [9936543414]   # شماره ادمین
 
 CLINIC_NAME = "کلینیک زیبایی تمارا"
-CLINIC_ADDRESS = "دماوند - خیابان بهشتی"
+CLINIC_ADDRESS = "دماوند، خیابان بهشتی"
+
 WHATSAPP_NUMBER = "989190432181"
-INSTAGRAM_URL = "https://instagram.com/tamana_clinic"
-
-# پرداخت
-MERCHANT_ID = "test"
-PAYMENT_CALLBACK = "/nextpay/callback"
-
-# کارت به کارت
-CARD_NUMBER = "6037-9918-1234-5678"
+INSTAGRAM_URL = "https://instagram.com/testclinic"
+CARD_NUMBER = "6037991234567890"
